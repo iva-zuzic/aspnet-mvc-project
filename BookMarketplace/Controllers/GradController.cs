3a4d6f2131ccdcfe3698e2bb98c326dc4699599d
Iva Zuzic <iva444@gmail.com>
2026-04-16 09:36:47 +0200

    Dodani Index i Details za Grad

--- BookMarketplace/Controllers/GradController.cs
+++ BookMarketplace/Controllers/GradController.cs
@@ -1,29 +1,42 @@
 using System.Collections.Generic;
+using System.Linq;
 using BookMarketplace.MockRepositories;
+using BookMarketplace.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookMarketplace.Controllers
 {
     public class GradController : Controller
     {
-        private readonly GradMockRepository _repository;
+        private readonly GradMockRepository _gradRepository;
+        private readonly OglasMockRepository _oglasRepository;
 
-        public GradController(GradMockRepository repository)
+        public GradController(GradMockRepository gradRepository, OglasMockRepository oglasRepository)
         {
-            _repository = repository;
+            _gradRepository = gradRepository;
+            _oglasRepository = oglasRepository;
         }
 
         public IActionResult Index()
         {
-            var gradovi = _repository.GetAll();
+            var gradovi = _gradRepository.GetAll();
+            var oglasi = _oglasRepository.GetAll();
+
+            foreach (var grad in gradovi)
+            {
+                grad.Oglasi = oglasi.Where(o => o.GradId == grad.Id).ToList();
+            }
+
             return View(gradovi);
         }
 
         public IActionResult Details(int id)
         {
-            var grad = _repository.GetById(id);
+            var grad = _gradRepository.GetById(id);
             if (grad is null)
                 return NotFound();
+
+            grad.Oglasi = _oglasRepository.GetAll().Where(o => o.GradId == grad.Id).ToList();
 
             return View(grad);
         }
